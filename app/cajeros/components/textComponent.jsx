ad875b529f0c433fd7e102153759a59a25f20e2a
DanielVargasHdz <danixasce17@gmail.com>
2024-07-05 12:17:08 -0600

    Corrección Front Catalogo Catalogos

--- app/cajeros/components/textComponent.jsx
+++ app/cajeros/components/textComponent.jsx
@@ -1,33 +1,32 @@
 import React from "react";
 
 function TextComponent({
-  titulo,
-  name,
-  message,
-  register,
-  errors,
-  requerido,
-  type,
+    titulo,
+    name,
+    message,
+    register,
+    errors,
+    requerido,
+    type,
 }) {
-  return (
-    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="text-slate-500 mb-2 block" htmlFor="username">
-        {titulo}
-      </label>
-      <input
-        type={type}
-        name={name}
-        className="p-3 rounded block bg-white-900 text-slate-900 w-full"
-        placeholder={titulo}
-        {...register(name, {
-          ...(requerido && { required: message }),
-        })}
-      />
-      {errors[name] && requerido && (
-        <span className="text-red-500 text-sm">{errors[name].message}</span>
-      )}
-    </div>
-  );
+    return (
+        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0 my-3">
+            <label className="input input-bordered flex items-center gap-2" htmlFor="username">
+                {titulo} : 
+            <input
+                type={type}
+                name={name}
+                className="grow"
+                {...register(name, {
+                    ...(requerido && { required: message }),
+                })}
+            />
+            </label>
+            {errors[name] && requerido && (
+                <span className="text-red-500 text-sm">{errors[name].message}</span>
+            )}
+        </div>
+    );
 }
 
 export default TextComponent;
