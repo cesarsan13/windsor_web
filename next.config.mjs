3f2f8e6e5bbb13b259d17fde13e17aee28bf1c32
cesarsan13 <cesaromarsancheztapia@gmail.com>
2024-07-15 22:42:05 +0000

    cambios par ruteo

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,7 @@ const nextConfig = {
   basePath: "/windsor",
   env: {
     DOMAIN_API: process.env.DOMAIN_API,
-  },
-  async redirects() {
-    return [
-      {
-        source: "/windsor",
-        destination: "/windsor/auth/login",
-        permanent: true,
-      },
-    ];
-  },
+  },	
 };
 
 export default nextConfig;
