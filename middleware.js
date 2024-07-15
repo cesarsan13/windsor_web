24ae7870b4fd4c0556b5bcfad53615e4ffb72d81
cesarsan13 <cesaromarsancheztapia@gmail.com>
2024-07-15 19:39:04 +0000

    asd

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
 export { default } from "next-auth/middleware";
 export const config = {
-  matcher: ["/", "/windsor/formapago", "/windsor/productos"],
+  matcher: ["/","/windsor/", "/windsor/formapago", "/windsor/productos"],
 };
