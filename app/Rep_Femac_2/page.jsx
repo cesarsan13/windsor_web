cd451ee666c64b86e66237b007370fde0c0bd406
AndreaMejia3 <andreamejia3333@outlook.com>
2024-07-24 17:32:05 -0600

    Vista rep_femac_2 ya esta lista, cambio en reportespdf agregue pageBreakH para el salto de pagina horizontal 24-07-2024

--- app/Rep_Femac_2/page.jsx
+++ app/Rep_Femac_2/page.jsx
@@ -2,107 +2,49 @@
 import React from "react"
 import { useRouter } from "next/navigation";
 import Acciones from "./components/Acciones";
-import { useForm } from "react-hook-form";
 import { 
   ImprimirPDF,
   ImprimirExcel,
-  getHorariosAPC,
+  getRepDosSel,
 } from "../utils/api/Rep_Femac_2/Rep_Femac_2";
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
-import jsPDF from "jspdf";
 import { Worker, Viewer } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import { ReportePDF } from "@/app/utils/ReportesPDF";
 import "jspdf-autotable";
-import * as XLSX from "xlsx";
-
 import BuscarCat from "../components/BuscarCat";
-import ModalHorario from "@/app/horarios/components/ModalHorario";
 
 function AlumnosPorClase(){
   const router = useRouter();
   const {data: session, status} = useSession();
-  const [isLoading, setisLoading] = useState(false);
-  const [formaHorarioAPC, setFormaHorarioAPC] = useState([]);
-  //const [shorario1, ssethorario1] = useState('');
-  //const [shorario2, ssethorario2] = useState('');
   const [sOrdenar, ssetordenar] = useState('');
   const [FormaRepDosSel, setFormaRepDosSel] =  useState([]);
-  const [accion, setAccion] = useState("");
   const [pdfPreview, setPdfPreview] = useState(false);
   const [pdfData, setPdfData] = useState("");
-  const [reporte, setReporte] = useState({});
-  const [reportes, setReportes] = useState([]);
-
-
-  const [data, setData] = useState({});
-  const [da, setDa] = useState({});
-  const [dia, setDia] = useState("");
-  const [openModal, setModal] = useState(false);
-  const [horarios, setHorarios] = useState([]);
-  const [horario, setHorario] = useState({});
-  const [currentID, setCurrentId] = useState("");
+  const [isLoading, setisLoading] = useState(false);
+  //guarda el valor
+  const [horario1, setHorario1] = useState({});
+  const [horario2, setHorario2] = useState({});
 
   useEffect(()=> {
     if(status === "loading" || !session) {
       return;
     }
-    
     const fetchData = async () => {
       setisLoading(true);
-      const { token } = session.user;
-      const data = await getHorariosAPC(token);
-      setFormaHorarioAPC(data);
-      setisLoading(false); 
-    };
-    
-    fetchData();
-    
-  }, [session, status]);
-
-  const {
-    register,
-    handleSubmit,
-    reset,
-    control,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      numero: reporte.numero,
-      numero_1: reporte.numero_1,
-      nombre_1: reporte.nombre_1,
-      año_nac_1: reporte.año_nac_1,
-      mes_nac_1: reporte.mes_nac_1,
-      telefono_1: reporte.telefono_1,
-      numero_2: reporte.numero_2,
-      nombre_2: reporte.nombre_2,
-      año_nac_2: reporte.año_nac_2,
-      mes_nac_2: reporte.mes_nac_2,
-      telefono_2: reporte.telefono_2,
-    },
-  });
-
-  const onSubmitModal = handleSubmit(async (data) => {
-    event.preventDefault;
-    const dataj = JSON.stringify(data);
-    data.id = currentID;
-    let res = null;
-    res = await guardaRep(session.user.token, data, accion);
-    if (res.status) {
-      if (accion === "Alta") {
-        const nuevaRep = { currentID, ...data };
-        setReportes([...reportes, nuevaRep]);
-        if (!bajas) {
-          setFormaRepDosSel([...FormaRepDosSel, nuevaRep]);
-        }
-      }
-      showSwal(res.alert_title, res.alert_text, res.alert_icon);
-      showModal(false);
+      const { token } = session.user
+      const data = await getRepDosSel(token, horario1, horario2, sOrdenar);
+      setFormaRepDosSel(data.data);
+      setisLoading(false);
     }
-  });
+    fetchData()
+  }, [session, status, horario1, horario2, sOrdenar]);
 
-  
+  const home = () => {
+    router.push("/");
+  };
+
   const ImprimePDF = () => {
     const configuracion = {
       Encabezado:{
@@ -111,54 +53,39 @@ function AlumnosPorClase(){
         Nombre_Usuario: `Usuario: ${session.user.name}`,
       },
       body: FormaRepDosSel,
-
     }
     ImprimirPDF(configuracion)
   }
 
   const ImprimeExcel = () => {
-
     const configuracion = {
       Encabezado:{
         Nombre_Aplicacion: "Sistema de Control Escolar",
         Nombre_Reporte: "Reporte de Alumnos por clase",
         Nombre_Usuario: `Usuario: ${session.user.name}`,
       },
-      
-      body: FormaRepDosSel1,
+      body: FormaRepDosSel,
       columns:[
-        { header: "No.", dataKey: "numero" },
-        { header: "Nombre", dataKey: "nombre" },
-        { header: "No. 1", dataKey: "id" },
-        { header: "Año", dataKey: "año_nac_1" },
-        { header: "Mes", dataKey: "mes_nac_1" },
-        { header: "Telefono", dataKey: "telefono_1" },
-        { header: "Nombre", dataKey: "nombre_2" },
-        { header: "No. 2", dataKey: "numero_2" },
-        { header: "Año", dataKey: "año_nac_2" },
-        { header: "Mes", dataKey: "mes_nac_2" },
-        { header: "Telefono", dataKey: "telefono_2" },
-
+        { header: "No.", dataKey: "Num_Renglon" },
+        { header: "No. 1", dataKey: "Numero_1" },
+        { header: "Nombre", dataKey: "Nombre_1" },
+        { header: "Año", dataKey: "Año_Nac_1" },
+        { header: "Mes", dataKey: "Mes_Nac_1" },
+        { header: "Telefono", dataKey: "Telefono_1" },
+        { header: "No. 2", dataKey: "Numero_2" },
+        { header: "Nombre", dataKey: "Nombre_2" },
+        { header: "Año", dataKey: "Año_Nac_2" },
+        { header: "Mes", dataKey: "Mes_Nac_2" },
+        { header: "Telefono", dataKey: "Telefono_2" },
     ],
-
     nombre: "RepDosSec"
   }
-  ImprimirExcel(configuracion)
-}
-
-  const home = () => {
-    router.push("/");
-  };
-
-  const handleSelectionChange1 = (event) =>{
-    event.preventDefault;    
-    ssethorario1(event.target.value);
-    console.log(event.target.value);
+    ImprimirExcel(configuracion)
   }
-  const handleSelectionChange2 = (event) =>{
-    event.preventDefault;    
-    ssethorario2(event.target.value);
-    console.log(event.target.value);
+
+  const handleCheckChange = (event) =>{
+    event.preventDefault;
+    ssetordenar(event.target.value);
   }
 
   const handleVerClick = () => {
@@ -171,23 +98,22 @@ function AlumnosPorClase(){
       body: FormaRepDosSel,
     };
     const reporte = new ReportePDF(configuracion, "Landscape");
-    
+    const { body } = configuracion;
     const Enca1 = (doc) => {
       if (!doc.tiene_encabezado) {
         doc.imprimeEncabezadoPrincipalH();
         doc.nextRow(12);
         doc.ImpPosX("No.",15,doc.tw_ren),
-        doc.ImpPosX("Nombre",30,doc.tw_ren),
-        doc.ImpPosX("No. 1",80,doc.tw_ren),
-        doc.ImpPosX("Año",95,doc.tw_ren),
-        doc.ImpPosX("Mes",110,doc.tw_ren),
-        doc.ImpPosX("Telefono",130,doc.tw_ren),
-        doc.ImpPosX("Nombre",155,doc.tw_ren),
-        doc.ImpPosX("No. 2",205,doc.tw_ren),
-        doc.ImpPosX("Año",220,doc.tw_ren),
-        doc.ImpPosX("Mes",235,doc.tw_ren),
-        doc.ImpPosX("Telefono",250,doc.tw_ren),
-
+        doc.ImpPosX("No. 1",25,doc.tw_ren),
+        doc.ImpPosX("Nombre",35,doc.tw_ren),
+        doc.ImpPosX("Año",120,doc.tw_ren),
+        doc.ImpPosX("Mes",130,doc.tw_ren),
+        doc.ImpPosX("Telefono",140,doc.tw_ren),
+        doc.ImpPosX("No. 2",155,doc.tw_ren),
+        doc.ImpPosX("Nombre",165,doc.tw_ren),
+        doc.ImpPosX("Año",250,doc.tw_ren),
+        doc.ImpPosX("Mes",260,doc.tw_ren),
+        doc.ImpPosX("Telefono",270,doc.tw_ren),
         doc.nextRow(4);
         doc.printLineH();
         doc.nextRow(4);
@@ -199,65 +125,36 @@ function AlumnosPorClase(){
     };
 
     Enca1(reporte);
-    configuracion.body.data1.foreach((reporte) => {
-      //reporte.ImpPosX(reporte.numero.toString(),15,reporte.tw_ren, 10);
-      reporte.ImpPosX(reporte.nombre.toString(),30,reporte.tw_ren, 50);
-      reporte.ImpPosX(reporte.id.toString(),80,reporte.tw_ren, 10);
-      reporte.ImpPosX(reporte.año_nac.toString(),95,reporte.tw_ren, 15);
-      reporte.ImpPosX(reporte.mes_nac.toString(),110,reporte.tw_ren, 15);
-      reporte.ImpPosX(reporte.telefono_1.toString(),130,reporte.tw_ren, 10);
-     
+    body.forEach((reporte1) => {
+      reporte.ImpPosX(reporte1.Num_Renglon.toString(),15, reporte.tw_ren);
+      reporte.ImpPosX(reporte1.Numero_1.toString(),25, reporte.tw_ren);
+      reporte.ImpPosX(reporte1.Nombre_1.toString(),35, reporte.tw_ren);
+      reporte.ImpPosX(reporte1.Año_Nac_1.toString().substring(0,4),120, reporte.tw_ren);
+      reporte.ImpPosX(reporte1.Mes_Nac_1.toString().substring(4,2),130, reporte.tw_ren);
+      reporte.ImpPosX(reporte1.Telefono_1.toString(),140, reporte.tw_ren);
+      reporte.ImpPosX(reporte1.Numero_2.toString(),155, reporte.tw_ren);
+      reporte.ImpPosX(reporte1.Nombre_2.toString(),165, reporte.tw_ren);
+      reporte.ImpPosX(reporte1.Año_Nac_2.toString().substring(0,4),250, reporte.tw_ren);
+      reporte.ImpPosX(reporte1.Mes_Nac_2.toString().substring(4,2),260, reporte.tw_ren);
+      reporte.ImpPosX(reporte1.Telefono_2.toString(),270, reporte.tw_ren);
       Enca1(reporte);
-      if (reporte.tw_ren >= reporte.tw_endRen) {
-        reporte.pageBreak();
+      if (reporte.tw_ren >= reporte.tw_endRenH) {
+        reporte.pageBreakH();
         Enca1(reporte);
       }
     });
-    configuracion.body.data2.foreach((reporte) => {
-      reporte.ImpPosX(reporte.nombre.toString(),155,reporte.tw_ren, 50);
-      reporte.ImpPosX(reporte.id.toString(),205,reporte.tw_ren, 10);
-      reporte.ImpPosX(reporte.año_nac.toString(),220,reporte.tw_ren, 15);
-      reporte.ImpPosX(reporte.mes_nac.toString(),235,reporte.tw_ren, 15);
-      reporte.ImpPosX(reporte.telefono_1.toString(),250,reporte.tw_ren, 10);
-      Enca1(reporte);
-      if (reporte.tw_ren >= reporte.tw_endRen) {
-        reporte.pageBreak();
-        Enca1(reporte);
-      }
-    });
-
     const pdfData = reporte.doc.output("datauristring");
     setPdfData(pdfData);
     setPdfPreview(true);
   };
 
-  const handleCheckChange = (event) =>{
-    event.preventDefault;
-    ssetordenar(event.target.value);
-    console.log(event.target.value);
-  }
-  
   if (status === "loading") {
     return (
       <div className="container skeleton    w-full  max-w-screen-xl  shadow-xl rounded-xl "></div>
     );
   }
-
-  const fieldsToShow = ["numero", "horario"];
-
   return (
     <>
-    <ModalHorario
-        accion={accion}
-        onSubmit={onSubmitModal}
-        currentID={currentID}
-        errors={errors}
-        register={register}
-        setHorarios={setHorario}
-        horarios={horario}
-        control={control}
-        setDia={setDia}
-      />
       <div className="container  w-full  max-w-screen-xl bg-slate-100 dark:bg-slate-700 shadow-xl rounded-xl px-3 ">
         <div className="flex justify-start p-3 ">
           <h1 className="text-4xl font-xthin text-black dark:text-white md:px-12">
@@ -266,20 +163,47 @@ function AlumnosPorClase(){
         </div>
         <div className="container grid grid-cols-8 grid-rows-1 h-[calc(100%-20%)] ">
           <div className="col-span-1 flex flex-col ">
-            <Acciones Ver={handleVerClick} ImprimePDF={ImprimePDF} ImprimeExcel={ImprimeExcel} home={home}></Acciones>
+            <Acciones 
+              Ver={handleVerClick} 
+              ImprimePDF={ImprimePDF}
+              ImprimeExcel={ImprimeExcel}
+              home={home}>  
+            </Acciones>
           </div>
         
         <div className="col-span-7">
         <div className="flex flex-col h-[calc(100%)]">
               <BuscarCat
                 table="horarios"
-                titulo={"horarios: "}
+                titulo={"horario 1: "}
                 token={session.user.token}
-                fieldsToShow={fieldsToShow}
-                setItem={setDa}
+                nameInput={["horario_1", "horario_1_nombre"]}
+                fieldsToShow={["numero", "horario"]}
+                setItem={setHorario1}
                 modalId="modal_horarios"
               />
-
+              <BuscarCat
+                table="horarios"
+                titulo={"horario 2: "}
+                token={session.user.token}
+                nameInput={["horario_2", "horario_2_nombre"]}
+                fieldsToShow={["numero", "horario"]}
+                setItem={setHorario2}
+                modalId="modal_horarios2"
+              />
+              <div className=" col-8">
+                <label className={` input-md text-black dark:text-white flex items-center gap-3`}>
+                    <span className="text-black dark:text-white">Ordenar por:</span>
+                    <label className={` input-md text-black dark:text-white flex items-center gap-3`} onChange={(event) => handleCheckChange(event)} >
+                        <span className="text-black dark:text-white">Nombre</span>
+                        <input type="radio" name="ordenar" value="nombre" className="radio" />
+                    </label>
+                    <label className={` input-md text-black dark:text-white flex items-center gap-3`} onChange={(event) => handleCheckChange(event)}>
+                        <span className="text-black dark:text-white">Número</span>
+                        <input type="radio" name="ordenar" value="id" className="radio" />
+                    </label>
+                </label>
+              </div>
             {pdfPreview && pdfData && (
                 <div className="pdf-preview">
                   <Worker
@@ -297,7 +221,6 @@ function AlumnosPorClase(){
       </div>
     </>
   );
-
 }
 
 export default AlumnosPorClase;
