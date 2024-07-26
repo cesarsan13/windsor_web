755aff3ab28c9b235625a3b181993f873f693f7a
DanielVargasHdz <danixasce17@gmail.com>
2024-07-26 10:31:28 -0600

    20240726 Correcciones del modo Claro/Negro

--- app/formfact/components/Busqueda.jsx
+++ app/formfact/components/Busqueda.jsx
@@ -21,14 +21,14 @@ function Busqueda({
     <div className="join w-full max-w-3/4 flex justify-start items-center h-1/8 p-1">
       <input
         id="TB_Busqueda"
-        className="input input-bordered input-md join-item w-full max-w-lg dark:bg-slate-100 "
+        className="input input-bordered input-md join-item w-full max-w-lg dark:bg-[#191e24] dark:text-neutral-200 text-neutral-600 "
         placeholder="Buscar..."
         onChange={(event) => handleBusquedaChange(event)}
         onKeyDown={(evt) => handleKeyDown(evt)}
         value={TB_Busqueda}
       />
       <select
-        className="select select-bordered join-item dark:bg-slate-100 w-20 md:w-32"
+        className="select select-bordered join-item dark:bg-[#191e24] dark:text-neutral-200 w-20 md:w-32 text-neutral-600"
         onChange={(event) => handleFiltroChange(event)}
       >
         <option disabled defaultValue={""}>
@@ -53,8 +53,8 @@ function Busqueda({
             className=" checkbox mx-2 checkbox-md"
             onClick={(evt) => setBajas(evt.target.checked)}
           />
-          <span className="fas fa-trash block sm:hidden md:hidden lg:hidden xl:hidden"></span>
-          <span className="label-text font-bold md:block hidden ">Bajas</span>
+          <span className="fa-solid fa-trash block sm:hidden md:hidden lg:hidden xl:hidden text-neutral-600 dark:text-neutral-200"></span>
+          <span className="label-text font-bold md:block hidden text-neutral-600 dark:text-neutral-200">Bajas</span>
         </label>
       </div>
       <div className="ml-2 p-2 flex justify-between">
--- app/formfact/page.jsx
+++ app/formfact/page.jsx
@@ -238,9 +238,9 @@ function FormFact() {
         setFormFact={setFormFact}
         formFact={formFact}
       />
-      <div className="container  w-full  max-w-screen-xl bg-slate-100 shadow-xl rounded-xl px-3 ">
-        <div className="flex justify-start p-3 ">
-          <h1 className="text-4xl font-xthin text-black md:px-12">
+      <div className="container  w-full  max-w-screen-xl bg-slate-100 dark:bg-slate-700 shadow-xl rounded-xl px-3 ">
+        <div className="flex justify-start p-3">
+          <h1 className="text-4xl font-xthin text-black dark:text-white md:px-12">
             Formas Facturas.
           </h1>
         </div>
