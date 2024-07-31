5b703116d9c36390d7050ad66b11ca1e4aa49571
jose luis <carrerasoto53@gmail.com>
2024-07-31 16:12:36 -0600

    20240731_Cambios_Vista_Previa_Horarios

--- app/horarios/components/Acciones.jsx
+++ app/horarios/components/Acciones.jsx
@@ -2,7 +2,7 @@ import Button from '@/app/components/button'
 import Tooltip from '@/app/components/tooltip'
 import React from 'react'
 
-function Acciones({Buscar,Alta,home,PDF,Excel}) {
+function Acciones({Buscar,Alta,home,Ver}) {
   return (
     <div className="join join-vertical ">
       <Tooltip Titulo={"Buscar"} posicion={"tooltip-top"}>
@@ -14,11 +14,8 @@ function Acciones({Buscar,Alta,home,PDF,Excel}) {
       <Tooltip Titulo={"Alta"} posicion={"tooltip-top"}>
         <Button icono={"fa-regular fa-square-plus"} onClick={Alta}></Button>
       </Tooltip>
-      <Tooltip Titulo={"PDF"} posicion={"tooltip-top"}>
-        <Button icono={"fa-solid fa-file-pdf"} onClick={PDF}></Button>
-      </Tooltip>
-      <Tooltip Titulo={"Excel"} posicion={"tooltip-top"}>
-        <Button icono={"fa-solid fa-file-excel"} onClick={Excel}></Button>
+      <Tooltip Titulo={"Vista Previa"} posicion={"tooltip-top"}>
+        <Button icono={"fas fa-eye"} onClick={Ver}></Button>
       </Tooltip>
       <Tooltip Titulo={"Inicio"} posicion={"tooltip-top"}>
         <Button icono={"fas fa-home"} onClick={home}></Button>
