2c17719269947814cc018e29eb3ce7bc9ef1782a
DanielVargasHdz <danixasce17@gmail.com>
2024-08-12 12:42:52 -0600

    20240812 Cambios Vista Movil Comentarios

--- app/comentarios/components/Acciones.jsx
+++ app/comentarios/components/Acciones.jsx
@@ -3,7 +3,7 @@ import Tooltip from "@/app/components/tooltip";
 import Button from "@/app/components/button";
 function Acciones({ Buscar, Alta, home, Ver}) {
   return (
-    <div className="join join-vertical ">
+    <div className="join join-horizontal justify-around md:join-vertical">
       <Tooltip Titulo={"Buscar"} posicion={"tooltip-top"}>
         <Button icono={"fa-solid fa-magnifying-glass"} onClick={Buscar}></Button>
       </Tooltip>
