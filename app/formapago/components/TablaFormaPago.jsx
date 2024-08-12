5f7deb42e290eddb5430762f0881a30cac699c5f
DanielVargasHdz <danixasce17@gmail.com>
2024-08-12 12:49:13 -0600

    20240812 Cambios Vista Movil Cajeros y FormaPago

--- app/formapago/components/TablaFormaPago.jsx
+++ app/formapago/components/TablaFormaPago.jsx
@@ -18,7 +18,7 @@ function TablaFormaPago({
   };
 
   return !isLoading ? (
-    <div className="overflow-x-auto mt-3 h-[calc(75vh)] text-black bg-white dark:bg-[#1d232a] dark:text-white m-2 w-full lg:w-3/4">
+    <div className="overflow-x-auto mt-3 h-[calc(55vh)] text-black bg-white dark:bg-[#1d232a] dark:text-white w-full lg:w-3/4">
       {formaPagosFiltrados.length > 0 ? (
         <table className="table table-xs table-zebra w-full">
           <thead className="sticky top-0 bg-white dark:bg-[#1d232a]">
