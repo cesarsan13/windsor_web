230797e2abf1c037d3a1cb5f901efbd0657e07ac
DanielVargasHdz <danixasce17@gmail.com>
2024-09-27 16:32:38 -0600

    Correccion de Catalogos Productos y Cajeros

--- app/productos/components/tablaProductos.jsx
+++ app/productos/components/tablaProductos.jsx
@@ -19,7 +19,7 @@ function TablaProductos({
     return !isLoading ? (
         <div className="overflow-y-auto mt-3 h-[calc(55vh)] md:h-[calc(65vh)] text-black bg-white dark:bg-[#1d232a] dark:text-white  w-full lg:w-full">
             {productosFiltrados.length > 0 ? (
-                <table className='table table-sm table-zebra w-full'>
+                <table className='table table-xs table-zebra w-full'>
                     <thead className="sticky top-0 bg-white dark:bg-[#1d232a] z-[2]">
                         <tr>
                             <th></th>
