7193c0e7caa506925b8318c6d8970e1409e41343
Abel <lujanlom0120@gmail.com>
2024-10-02 10:00:45 -0600

    cambios en reporte alumnos inscritos

--- app/rep_inscritos/components/ModalVistaPreviaRepInsc.jsx
+++ app/rep_inscritos/components/ModalVistaPreviaRepInsc.jsx
@@ -1,15 +1,19 @@
+'use client'
 import Tooltip from '@/app/components/tooltip';
-import { Viewer, Worker } from '@react-pdf-viewer/core'
+import { SpecialZoomLevel, Viewer, Worker } from '@react-pdf-viewer/core'
 import React, { useEffect, useState } from 'react'
-import '@react-pdf-viewer/core/lib/styles/index.css';
-
-function ModalVistaPreviaRepInsc({ pdfPreview, pdfData, PDF, Excel }) {
+import Image from 'next/image';
+import iconos from '@/app/utils/iconos';
+import "@react-pdf-viewer/core/lib/styles/index.css";
+function ModalVistaPreviaRep6({ pdfPreview, pdfData, PDF, Excel }) {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     useEffect(() => {
+        // Determinar si el modal debe abrirse
         const isEmptyDataPDF = Object.keys(pdfData || {}).length === 0 && (pdfData || {}).constructor === Object;
         setIsModalOpen(pdfPreview && !isEmptyDataPDF);
     }, [pdfPreview, pdfData]);
+
     return (
         <dialog id='modalVPRepInsc' className='modal'>
             <div className='modal-box w-full max-w-4xl h-full'>
@@ -18,18 +22,30 @@ function ModalVistaPreviaRepInsc({ pdfPreview, pdfData, PDF, Excel }) {
                 >
                     ✕
                 </button>
-                <h3 className='font-bold text-lg mb-5 dark:text-white text-black'>Vista Previa Alumnos</h3>
+                <h3 className='font-bold text-lg mb-5 dark:text-white text-black'>Vista Previa Alumnos Inscritos</h3>
                 <div className='flex flex-row space-x-4'>
                     <Tooltip Titulo={"Imprimir PDF"} posicion={"tooltip-top"}>
-                        <button className='hover:bg-transparent border-none shadow-md hover:bg-slate-200 dark:hover:bg-neutral-700 bg-transparent text-black dark:text-white rounded-lg btn' onClick={PDF}>Imprimir PDF<i className='fa-solid fa-file-pdf'></i></button>
+                        <button
+                            className="bg-transparent over:bg-slate-200 dark:hover:bg-neutral-700 text-neutral-600 dark:text-white rounded-lg btn"
+                            onClick={PDF}
+                        >
+                            <span className="hidden sm:inline">Generar PDF</span>
+                            <Image src={iconos.imprimir} alt="Imprimir" className="w-5 h-5 md:w-6 md:h-6" />
+                        </button>
                     </Tooltip>
                     <Tooltip Titulo={"Imprimir Excel"} posicion={"tooltip-top"}>
-                        <button className='hover:bg-transparent border-none shadow-md hover:bg-slate-200 dark:hover:bg-neutral-700 bg-transparent text-black dark:text-white rounded-lg btn' onClick={Excel}>Imprimir Excel<i className='fa-solid fa-file-excel'></i></button>
+                        <button
+                            className="bg-transparent over:bg-slate-200 dark:hover:bg-neutral-700 text-neutral-600 dark:text-white rounded-lg btn"
+                            onClick={Excel}
+                        >
+                            <span className="hidden sm:inline">Generar Excel</span>
+                            <Image src={iconos.excel} alt="Excel" className="w-5 h-5 md:w-6 md:h-6" />
+                        </button>
                     </Tooltip>
                 </div>
                 {pdfPreview && pdfData && (
-                    <div className='w-full'>
-                        <div className='pdf-preview'>
+                    <div className='mt-4'>
+                        <div className='pdf-preview' >
                             <Worker
                                 workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}
                             >
@@ -42,7 +58,7 @@ function ModalVistaPreviaRepInsc({ pdfPreview, pdfData, PDF, Excel }) {
                 )}
             </div>
         </dialog>
-    )
+    );
 }
 
-export default ModalVistaPreviaRepInsc
+export default ModalVistaPreviaRep6
