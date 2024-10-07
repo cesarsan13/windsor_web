584e653d1fc6fa374f398b27f1e27f9bc347aeb2
DanielVargasHdz <danixasce17@gmail.com>
2024-10-07 10:37:38 -0600

    Actualizacion de Catalogos

--- app/horarios/page.jsx
+++ app/horarios/page.jsx
@@ -332,7 +332,7 @@ function Horarios() {
               />
             </div>
 
-            <h1 className="order-1 md:order-2 text-4xl font-xthin text-black dark:text-white mb-5 md:mb-0 grid grid-flow-col gap-1 justify-around w-2/12">
+            <h1 className="order-1 md:order-2 text-4xl font-xthin text-black dark:text-white mb-5 md:mb-0 grid grid-flow-col gap-1 justify-around mx-5">
               Horarios
           </h1>
         </div>
