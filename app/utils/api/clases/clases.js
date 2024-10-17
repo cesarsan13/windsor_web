a133ae35ba307e6447b6562d59d64c53e81792bf
DanielVargasHdz <danixasce17@gmail.com>
2024-10-17 09:27:39 -0600

    Creacion del Proceso de Clases (Asignacion por Asignatura)

---
+++ app/utils/api/clases/clases.js
@@ -0,0 +1,55 @@
+import { useSession } from "next-auth/react";
+
+export const getClases = async (token, baja) => {
+    let url = "";
+    baja
+      ? (url = `${process.env.DOMAIN_API}api/clase/baja`)
+      : (url = `${process.env.DOMAIN_API}api/clase/`);
+  
+    const res = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const resJson = await res.json();
+    return resJson.data;
+  };
+
+  export const guardaClase = async (token, data, accion) => {
+    let url_api = "";
+    if (accion === "Alta") {
+      url_api = `${process.env.DOMAIN_API}api/clase/`;
+      data.baja = "";
+    }
+    if (accion === "Eliminar" || accion === "Editar") {
+      if (accion === "Eliminar") {
+        data.baja = "*";
+      } else {
+        data.baja = "";
+      }
+      url_api = `${process.env.DOMAIN_API}api/clase/updateClase/`;
+    }
+  
+    const res = await fetch(`${url_api}`, {
+      method: "post",
+      body: JSON.stringify({
+        materia: data.materia,
+        grupo: data.grupo,
+        profesor: data.profesor,
+        lunes: data.lunes,
+        martes: data.martes,
+        miercoles: data.miercoles,
+        jueves: data.jueves,
+        viernes: data.viernes,
+        sabado: data.sabado,
+        domingo: data.domingo,
+        baja: data.baja,
+      }),
+      headers: new Headers({
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      }),
+    });
+    const resJson = await res.json();
+    return resJson;
+  };
