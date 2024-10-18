9d67f8318125140da74f3e00f3c10f47589bb878
cesarsan13 <cesaromarsancheztapia@gmail.com>
2024-10-18 13:15:11 -0600

    prueba

--- app/comentarios/page.jsx
+++ app/comentarios/page.jsx
@@ -389,7 +389,7 @@ function Comentarios() {
             </div>
 
             <h1 className="order-1 md:order-2 text-4xl font-xthin text-black dark:text-white mb-5 md:mb-0 grid grid-flow-col gap-1 justify-around mx-5">
-              Comentarios
+              Comentarios.
             </h1>
           </div>
         </div>
