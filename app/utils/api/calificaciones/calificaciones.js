c6eb728b57b01984fb162d214777656fce958f66
Abel <lujanlom0120@gmail.com>
2024-11-01 13:06:56 -0600

    nuevas funciones en util calificaciones

--- app/utils/api/calificaciones/calificaciones.js
+++ app/utils/api/calificaciones/calificaciones.js
@@ -1,6 +1,6 @@
 import { ReporteExcel } from "@/app/utils/ReportesExcel";
 import { ReportePDF } from "@/app/utils/ReportesPDF";
-
+ 
 export const getProcesoCalificaciones = async (token, data) => {
     let url = `${process.env.DOMAIN_API}api/proceso/calificaciones-get`
     const res = await fetch(url, {
