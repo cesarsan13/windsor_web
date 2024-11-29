6ee5afbc7c6fff17fb2c323f786212ea03443c00
AndreaMejia3 <andreamejia3333@outlook.com>
2024-11-29 17:00:42 -0600

    implementacion de los permisos en actualizacion de cobranza 05:00pm 29/11/2024

--- app/act_cobranza/components/Acciones.jsx
+++ app/act_cobranza/components/Acciones.jsx
@@ -4,13 +4,35 @@ import Image from "next/image";
 import iconos from "@/app/utils/iconos";
 import React from 'react'
 
-function Acciones({ Buscar, Alta, home }) {
+function Acciones({ Buscar, Alta, home, permiso_alta }) {
     const images = [
-        { srcDark: iconos.buscar,srcLight:iconos.buscar_w, alt: 'Buscar', tooltipTitle: 'Buscar', onClick: Buscar },
-        { srcDark: iconos.alta,srcLight:iconos.alta_w, alt: 'Alta', tooltipTitle: 'Alta', onClick: Alta },        
-        { srcDark: iconos.salir,srcLight:iconos.salir_w, alt: 'Salir', tooltipTitle: 'Salir', onClick: home },
+        { 
+            srcDark: iconos.buscar,
+            srcLight:iconos.buscar_w, 
+            alt: 'Buscar', 
+            tooltipTitle: 'Buscar', 
+            onClick: Buscar,
+            permission: true
+        },
+        { 
+            srcDark: iconos.alta,
+            srcLight:iconos.alta_w, 
+            alt: 'Alta', 
+            tooltipTitle: 'Alta', 
+            onClick: Alta,
+            permission: permiso_alta
+        },        
+        { 
+            srcDark: iconos.salir,
+            srcLight:iconos.salir_w, 
+            alt: 'Salir', 
+            tooltipTitle: 'Salir', 
+            onClick: home,
+            permission: true
+        },
     ];
-    const ImageTooltip = ({ srcDark,srcLight, tooltipTitle, onClick }) => {
+    const ImageTooltip = ({ srcDark,srcLight, tooltipTitle, onClick, permission }) => {
+        if(!permission) return null;
         return (
             <Tooltip Titulo={tooltipTitle} posicion="tooltip-bottom">
                 <Image src={srcDark} alt={tooltipTitle} onClick={onClick} className="w-5 h-5 md:w-6 md:h-6 block dark:hidden" />
@@ -21,7 +43,7 @@ function Acciones({ Buscar, Alta, home }) {
     return (
         <div className="grid grid-flow-col gap-5 justify-around w-full">
             {images.map((image, idx) => (
-                <ImageTooltip key={idx} src={image.src} {...image} />
+                <ImageTooltip key={idx} src={image.src} {...image} permission={image.permission}/>
             ))}
         </div>
     )
