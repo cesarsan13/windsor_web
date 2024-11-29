3a2db79a06447565fc4d4bf4a5721285f73dc8a0
cesarsan13 <cesaromarsancheztapia@gmail.com>
2024-11-29 10:48:04 -0600

    cambios en acceso denegado

--- app/acceso_denegado/page.jsx
+++ app/acceso_denegado/page.jsx
@@ -1,29 +1,13 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { Suspense } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import AccessDenied from "@/app/components/AccessDenied";
 
 export default function Acceso_Denegado() {
-  const searchParams = useSearchParams();
-  const [menu, setMenu] = useState(false);
-  const router = useRouter();
-
-  useEffect(() => {
-    const isMenu = searchParams.get("menu") === "true";
-    setMenu(isMenu);
-
-    if (!isMenu) {
-      router.push("/");
-    }
-  }, [searchParams, router]);
-
-  if (!menu) {
-    return null;
-  }
-
   return (
-    <div>
-      <AccessDenied />
-    </div>
+    <Suspense fallback={<div>Cargando...</div>}>
+      <Acceso_Denegado />
+    </Suspense>
   );
 }
