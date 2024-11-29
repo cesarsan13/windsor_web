af9d2637ea5fcd5d9d5896a63bc0037192314e4c
Abel <lujanlom0120@gmail.com>
2024-11-29 13:20:05 -0600

    cambios de asignaturas permisos

--- app/asignaturas/components/tablaAsignaturas.jsx
+++ app/asignaturas/components/tablaAsignaturas.jsx
@@ -14,7 +14,33 @@ function TablaAsignaturas({
   setCurrentId,
   formatNumber,
   tableAction,
+  permiso_cambio,
+  permiso_baja,
 }) {
+  const ActionButton = ({ tooltip, iconDark, iconLight, onClick, permission }) => {
+    if (!permission) return null;
+    return (
+      <th>
+        <div
+          className="kbd pt-1 tooltip tooltip-left hover:cursor-pointer bg-transparent hover:bg-transparent text-black border-none shadow-none dark:text-white w-5 h-5 md:w-[1.80rem] md:h-[1.80rem] content-center"
+          data-tip={tooltip}
+          onClick={onClick}
+        >
+          <Image src={iconDark} alt={tooltip} className="block dark:hidden" />
+          <Image src={iconLight} alt={tooltip} className="hidden dark:block" />
+        </div>
+      </th>
+    );
+  };
+
+  const ActionColumn = ({ description, permission }) => {
+    if (!permission) return null;
+    return (
+      <>
+        <th className="w-[5%] pt-[.10rem] pb-[.10rem]">{description}</th>
+      </>
+    )
+  }
   return !isLoading ? (
     <div className="overflow-y-auto mt-3 h-[calc(55vh)] md:h-[calc(65vh)] text-black bg-white dark:bg-[#1d232a] dark:text-white  w-full lg:w-full">
       {asignaturasFiltrados && asignaturasFiltrados.length > 0 ? (
@@ -25,9 +51,18 @@ function TablaAsignaturas({
               <td className="w-[40%]">Asignatura</td>
               <td className="sm:w-[5%] pt-[.5rem] pb-[.5rem]">Area</td>
               <td className="sm:w-[5%] pt-[.5rem] pb-[.5rem]">Orden</td>
-              <th className="w-[5%] pt-[.10rem] pb-[.10rem]">Ver</th>
-              <th className="w-[5%] pt-[.10rem] pb-[.10rem]">Editar</th>
-              <th className="w-[5%] pt-[.10rem] pb-[.10rem]">Eliminar</th>
+              < ActionColumn
+                description={"Ver"}
+                permission={true}
+              />
+              < ActionColumn
+                description={"Editar"}
+                permission={permiso_cambio}
+              />
+              < ActionColumn
+                description={"Eliminar"}
+                permission={permiso_baja}
+              />
             </tr>
           </thead>
           <tbody>
@@ -49,36 +84,27 @@ function TablaAsignaturas({
                   typeof item.orden === "number" ? "text-right" : "text-left"
                 }
                 >{item.orden}</td>
-                <th className="w-[5%] pt-[.10rem] pb-[.10rem]">
-                  <div
-                    className="kbd pt-1 tooltip tooltip-left hover:cursor-pointer bg-transparent hover:bg-transparent text-black border-none shadow-none dark:text-white w-5 h-5 md:w-[1.80rem] md:h-[1.80rem] content-center"
-                    data-tip={`Ver`}
-                    onClick={(evt) => tableAction(`Ver`, item.numero)}
-                  >
-                    <Image src={iconos.ver} alt="Ver" className="block dark:hidden" />
-                    <Image src={iconos.ver_w} alt="Ver" className="hidden dark:block" />
-                  </div>
-                </th>
-                <th className="w-[5%] pt-[.10rem] pb-[.10rem]">
-                  <div
-                    className="kbd pt-1 tooltip tooltip-left hover:cursor-pointer bg-transparent hover:bg-transparent text-black border-none shadow-none dark:text-white w-5 h-5 md:w-[1.80rem] md:h-[1.80rem] content-center"
-                    data-tip={`Editar`}
-                    onClick={(evt) => tableAction(`Editar`, item.numero)}
-                  >
-                    <Image src={iconos.editar} alt="Editar" className="block dark:hidden" />
-                    <Image src={iconos.editar_w} alt="Editar" className="hidden dark:block" />
-                  </div>
-                </th>
-                <th className="w-[5%] pt-[.10rem] pb-[.10rem]">
-                  <div
-                    className="kbd pt-1 tooltip tooltip-left hover:cursor-pointer bg-transparent hover:bg-transparent text-black border-none shadow-none dark:text-white w-5 h-5 md:w-[1.80rem] md:h-[1.80rem] content-center"
-                    data-tip={`Eliminar`}
-                    onClick={(evt) => tableAction("Eliminar", item.numero)}
-                  >
-                    <Image src={iconos.eliminar} alt="Eliminar" className="block dark:hidden" />
-                    <Image src={iconos.eliminar_w} alt="Eliminar" className="hidden dark:block" />
-                  </div>
-                </th>
+                <ActionButton
+                  tooltip="Ver"
+                  iconDark={iconos.ver}
+                  iconLight={iconos.ver_w}
+                  onClick={(evt) => tableAction(evt, item, "Ver")}
+                  permission={true}
+                />
+                <ActionButton
+                  tooltip="Editar"
+                  iconDark={iconos.editar}
+                  iconLight={iconos.editar_w}
+                  onClick={(evt) => tableAction(evt, item, "Editar")}
+                  permission={permiso_cambio}
+                />
+                <ActionButton
+                  tooltip="Eliminar"
+                  iconDark={iconos.eliminar}
+                  iconLight={iconos.eliminar_w}
+                  onClick={(evt) => tableAction(evt, item, "Eliminar")}
+                  permission={permiso_baja}
+                />
               </tr>
             ))}
           </tbody>
