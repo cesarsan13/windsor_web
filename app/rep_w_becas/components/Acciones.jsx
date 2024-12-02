7c169bf3249e1e7ab4022734f03518180ea936b9
jose luis <carrerasoto53@gmail.com>
2024-12-02 12:17:13 -0600

    20241202_Correcciones_rep_w_becas

--- app/rep_w_becas/components/Acciones.jsx
+++ app/rep_w_becas/components/Acciones.jsx
@@ -1,17 +1,72 @@
 import React from "react";
 import Tooltip from "@/app/components/tooltip";
 import Button from "@/app/components/button";
-function Acciones({Ver, home }) {
+import iconos from "@/app/utils/iconos";
+import Image from "next/image";
+import { TbLoader3 } from "react-icons/tb";
+function Acciones({ Ver, home, permiso_imprime,isLoading }) {
+  const images = [
+    {
+      srcLight: iconos.vistaPrevia_w,
+      srcDark: iconos.vistaPrevia,
+      alt: 'Vista previa',
+      tooltipTitle: 'Vista previa',
+      onClick: Ver,
+      permission: permiso_imprime
+    },
+    {
+      srcLight: iconos.salir_w,
+      srcDark: iconos.salir,
+      alt: 'Salir',
+      tooltipTitle: 'Salir',
+      onClick: home,
+      permission: true
+    }
+  ]
+  const ImageTooltip = ({ srcDark, srcLight, tooltipTitle, onClick, isLoading, permission }) => {
+    if (!permission) return null
+    return (
+      <Tooltip Titulo={tooltipTitle} posicion="tooltip-bottom">
+        <button
+          className="w-6 h-5 bg-transparent hover:bg-transparent border-none shadow-none text-black dark:text-white rounded-lg"
+          onClick={onClick}
+        >
+          {isLoading ? (
+            <TbLoader3 className="animate-spin text-2xl" />
+          ) : (
+            <>
+              <Image
+                src={srcDark}
+                alt={tooltipTitle}
+
+                className="w-5 h-5 md:w-6 md:h-6 block dark:hidden"
+              />
+              <Image
+                src={srcLight}
+                alt={tooltipTitle}
+                className="w-5 h-5 md:w-6 md:h-6 hidden dark:block"
+              />
+            </>
+          )}
+        </button>
+      </Tooltip>
+    )
+  }
+
   return (
-    <div className="join join-vertical ">
-      <Tooltip Titulo={"Vista Previa"} posicion={"tooltip-top"}>
-        <Button icono={"fas fa-eye"} onClick={Ver}></Button>
-      </Tooltip>
-      <Tooltip Titulo={"Inicio"} posicion={"tooltip-top"}>
-        <Button icono={"fas fa-home"} onClick={home}></Button>
-      </Tooltip>
+    <div className="grid grid-flow-col gap-5 justify-around w-full">
+      {images.map((image, idx) => (
+        <ImageTooltip
+          key={idx}
+          srcLight={image.srcLight}
+          srcDark={image.srcDark}
+          tooltipTitle={image.tooltipTitle}
+          onClick={image.onClick}
+          isLoading={idx === 0 && isLoading}
+          permission={image.permission} />
+      ))}
     </div>
-  );
+  )
 }
 
 export default Acciones;
--- app/rep_w_becas/page.jsx
+++ app/rep_w_becas/page.jsx
@@ -2,7 +2,7 @@
 import React from "react"
 import { useRouter } from "next/navigation";
 import Acciones from "./components/Acciones";
-import { 
+import {
   ImprimirPDF,
   ImprimirExcel,
   getBecas,
@@ -16,134 +16,145 @@ import "jspdf-autotable";
 import BuscarCat from "../components/BuscarCat";
 import VistaPrevia from "../components/VistaPrevia";
 import { showSwal } from "@/app/utils/alerts";
+import { permissionsComponents } from "../utils/globalfn";
 
-function RepBecas(){
-    const router = useRouter();
-    const {data: session, status} = useSession();
-    const [sOrdenar, ssetordenar] = useState('nombre');
-    const [formaBecas, setFormaBecas] =  useState([]);
-    const [pdfPreview, setPdfPreview] = useState(false);
-    const [pdfData, setPdfData] = useState("");
-    const [isLoading, setisLoading] = useState(false);
-    //guarda el valor
-    const [horario1, setHorario1] = useState({});
-    const [horario2, setHorario2] = useState({});
-  
-    useEffect(()=> {
-      if(status === "loading" || !session) {
-        return;
-      }
-      const fetchData = async () => {
-        setisLoading(true);
-        const { token } = session.user
-        const data = await getBecas(token, horario1, horario2, sOrdenar);
-        console.log(data);
-        setFormaBecas(data.data);
-        setisLoading(false);
-      }
-      fetchData()
-    }, [session, status, horario1, horario2, sOrdenar]);
-    const home = () => {
-        router.push("/");
-      };
-      const CerrarView = () => {
-        setPdfPreview(false);
-        setPdfData("");
-        document.getElementById("modalVPRepWBecas").close();
+function RepBecas() {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+  const [sOrdenar, ssetordenar] = useState('nombre');
+  const [formaBecas, setFormaBecas] = useState([]);
+  const [pdfPreview, setPdfPreview] = useState(false);
+  const [pdfData, setPdfData] = useState("");
+  const [isLoading, setisLoading] = useState(false);
+  //guarda el valor
+  const [horario1, setHorario1] = useState({});
+  const [horario2, setHorario2] = useState({});
+  const [animateLoading, setAnimateLoading] = useState(false);
+  const [permissions, setPermissions] = useState({});
 
-    };
-    
-      const ImprimePDF = () => {
-        const configuracion = {
-          Encabezado:{
-            Nombre_Aplicacion: "Sistema de Control Escolar",
-            Nombre_Reporte: "Reporte de Becas",
-            Nombre_Usuario: `Usuario: ${session.user.name}`,
-          },
-          body: formaBecas,
-        }
-        ImprimirPDF(configuracion)
-      }
-      const ImprimeExcel = () => {
-        const configuracion = {
-          Encabezado:{
-            Nombre_Aplicacion: "Sistema de Control Escolar",
-            Nombre_Reporte: "Reporte de Becas",
-            Nombre_Usuario: `Usuario: ${session.user.name}`,
-          },
-          body: formaBecas,
-          columns:[
-            { header: "No.", dataKey: "numero" },
-            { header: "Nombre", dataKey: "alumno" },
-            { header: "Grado", dataKey: "grado" },
-            { header: "Beca", dataKey: "colegiatura" },
-            { header: "Colegiatura", dataKey: "descuento" },
-            { header: "Descuento", dataKey: "costo_final" },
-        ],
-        nombre: "ReporteBecas"
-      }
-        ImprimirExcel(configuracion)
-      }
-      const handleCheckChange = (event) =>{
-        ssetordenar(event.target.value);
-      }
-    
-      const handleVerClick = () => {
-        console.log(formaBecas);
-        if (horario1.numero === undefined && horario2.numero == undefined){
-          showSwal("Oppss!", "Para imprimir, mínimo debe estar seleccionada una fecha de 'Inicio'", "error");
-        } else {
-    
-        const configuracion = {
-          Encabezado: {
-            Nombre_Aplicacion: "Sistema de Control Escolar",
-            Nombre_Reporte: "Reporte de Becas",
-            Nombre_Usuario: `Usuario: ${session.user.name}`,
-          },
-          body: formaBecas,
-        };
-        const reporte = new ReportePDF(configuracion, "Landscape");
-    const { body } = configuracion;
-    const Enca1 = (doc) => {
-      if (!doc.tiene_encabezado) {
-        doc.imprimeEncabezadoPrincipalH();
-        doc.nextRow(12);
-        doc.ImpPosX("No.",15,doc.tw_ren),
-        doc.ImpPosX("Nombre",25,doc.tw_ren),
-        doc.ImpPosX("Grado",115,doc.tw_ren),
-        doc.ImpPosX("Colegiatura",160,doc.tw_ren),
-        doc.ImpPosX("Descuento",190,doc.tw_ren),
-        doc.ImpPosX("Saldo",210,doc.tw_ren),
-        doc.nextRow(4);
-        doc.printLineH();
-        doc.nextRow(4);
-        doc.tiene_encabezado = true;
-      } else {
-        doc.nextRow(6);
-        doc.tiene_encabezado = true;
-      }
-    };
-    Enca1(reporte);
-    console.log(body);
-    body.forEach((reporte1) => {
-      reporte.ImpPosX(reporte1.numero.toString(),15, reporte.tw_ren);
-      reporte.ImpPosX(reporte1.alumno.toString(),25, reporte.tw_ren);
-      reporte.ImpPosX(reporte1.grado.toString(),115, reporte.tw_ren);
-      reporte.ImpPosX(reporte1.colegiatura.toString().substring(0,4),160, reporte.tw_ren);
-      reporte.ImpPosX(reporte1.descuento.toString().substring(4,2),190, reporte.tw_ren);
-      reporte.ImpPosX(reporte1.costo_final.toString(),210, reporte.tw_ren);
-      //reporte.ImpPosX(reporte1.Saldo.toString(),155, reporte.tw_ren);
-      Enca1(reporte);
-      if (reporte.tw_ren >= reporte.tw_endRenH) {
-        reporte.pageBreakH();
-        Enca1(reporte);
-      }
-    });
-    const pdfData = reporte.doc.output("datauristring");
-    setPdfData(pdfData);
-    setPdfPreview(true);
-    showModalVista(true);
+  useEffect(() => {
+    if (status === "loading" || !session) {
+      return;
+    }
+    const fetchData = async () => {
+      setisLoading(true);
+      const { token } = session.user
+      let { permissions } = session.user
+      const es_admin = session.user.es_admin
+      const menuSeleccionado = Number(localStorage.getItem("puntoMenu"));
+      const permisos = permissionsComponents(es_admin, permissions, session.user.id, menuSeleccionado)
+      setPermissions(permisos)
+      const data = await getBecas(token, horario1, horario2, sOrdenar);
+      console.log(data);
+      setFormaBecas(data.data);
+      setisLoading(false);
+    }
+    fetchData()
+  }, [session, status, horario1, horario2, sOrdenar]);
+  const home = () => {
+    router.push("/");
+  };
+  const CerrarView = () => {
+    setPdfPreview(false);
+    setPdfData("");
+    document.getElementById("modalVPRepWBecas").close();
+
+  };
+
+  const ImprimePDF = () => {
+    const configuracion = {
+      Encabezado: {
+        Nombre_Aplicacion: "Sistema de Control Escolar",
+        Nombre_Reporte: "Reporte de Becas",
+        Nombre_Usuario: `Usuario: ${session.user.name}`,
+      },
+      body: formaBecas,
+    }
+    ImprimirPDF(configuracion)
   }
+  const ImprimeExcel = () => {
+    const configuracion = {
+      Encabezado: {
+        Nombre_Aplicacion: "Sistema de Control Escolar",
+        Nombre_Reporte: "Reporte de Becas",
+        Nombre_Usuario: `Usuario: ${session.user.name}`,
+      },
+      body: formaBecas,
+      columns: [
+        { header: "No.", dataKey: "numero" },
+        { header: "Nombre", dataKey: "alumno" },
+        { header: "Grado", dataKey: "grado" },
+        { header: "Beca", dataKey: "colegiatura" },
+        { header: "Colegiatura", dataKey: "descuento" },
+        { header: "Descuento", dataKey: "costo_final" },
+      ],
+      nombre: "ReporteBecas"
+    }
+    ImprimirExcel(configuracion)
+  }
+  const handleCheckChange = (event) => {
+    ssetordenar(event.target.value);
+  }
+
+  const handleVerClick = () => {
+    setAnimateLoading(true)
+    console.log(formaBecas);
+    if (horario1.numero === undefined && horario2.numero == undefined) {
+      showSwal("Oppss!", "Para imprimir, mínimo debe estar seleccionado un Horario", "error");
+      setAnimateLoading(false)
+    } else {
+
+      const configuracion = {
+        Encabezado: {
+          Nombre_Aplicacion: "Sistema de Control Escolar",
+          Nombre_Reporte: "Reporte de Becas",
+          Nombre_Usuario: `Usuario: ${session.user.name}`,
+        },
+        body: formaBecas,
+      };
+      const reporte = new ReportePDF(configuracion, "Landscape");
+      const { body } = configuracion;
+      const Enca1 = (doc) => {
+        if (!doc.tiene_encabezado) {
+          doc.imprimeEncabezadoPrincipalH();
+          doc.nextRow(12);
+          doc.ImpPosX("No.", 15, doc.tw_ren),
+            doc.ImpPosX("Nombre", 25, doc.tw_ren),
+            doc.ImpPosX("Grado", 115, doc.tw_ren),
+            doc.ImpPosX("Colegiatura", 160, doc.tw_ren),
+            doc.ImpPosX("Descuento", 190, doc.tw_ren),
+            doc.ImpPosX("Saldo", 210, doc.tw_ren),
+            doc.nextRow(4);
+          doc.printLineH();
+          doc.nextRow(4);
+          doc.tiene_encabezado = true;
+        } else {
+          doc.nextRow(6);
+          doc.tiene_encabezado = true;
+        }
+      };
+      Enca1(reporte);
+      console.log(body);
+      body.forEach((reporte1) => {
+        reporte.ImpPosX(reporte1.numero.toString(), 15, reporte.tw_ren);
+        reporte.ImpPosX(reporte1.alumno.toString(), 25, reporte.tw_ren);
+        reporte.ImpPosX(reporte1.grado === null ? "" : reporte1.grado.toString(), 115, reporte.tw_ren);
+        reporte.ImpPosX(reporte1.colegiatura.toString().substring(0, 4), 160, reporte.tw_ren);
+        reporte.ImpPosX(reporte1.descuento.toString().substring(4, 2), 190, reporte.tw_ren);
+        reporte.ImpPosX(reporte1.costo_final.toString(), 210, reporte.tw_ren);
+        //reporte.ImpPosX(reporte1.Saldo.toString(),155, reporte.tw_ren);
+        Enca1(reporte);
+        if (reporte.tw_ren >= reporte.tw_endRenH) {
+          reporte.pageBreakH();
+          Enca1(reporte);
+        }
+      });
+      const pdfData = reporte.doc.output("datauristring");
+      setPdfData(pdfData);
+      setPdfPreview(true);
+      showModalVista(true);
+      setAnimateLoading(false)
+    }
   };
   const showModalVista = (show) => {
     show
@@ -158,7 +169,7 @@ function RepBecas(){
   }
   return (
     <>
-    <VistaPrevia
+      <VistaPrevia
         id={"modalVPRepWBecas"}
         titulo={"Vista Previa de Reporte de Becas"}
         pdfPreview={pdfPreview}
@@ -166,9 +177,81 @@ function RepBecas(){
         PDF={ImprimePDF}
         Excel={ImprimeExcel}
         CerrarView={CerrarView}
-    />
+      />
 
-    <div className="container w-full max-w-screen-xl bg-base-200 dark:bg-slate-700 shadow-xl rounded-xl px-3">
+      <div className="container h-[80vh] w-full max-w-screen-xl bg-slate-100 dark:bg-slate-700 shadow-xl rounded-xl px-3 md:overflow-y-auto lg:overflow-y-hidden">
+        <div className="flex flex-col justify-start p-3">
+          <div className="flex flex-wrap md:flex-nowrap items-start md:items-center">
+            <div className="order-2 md:order-1 flex justify-around w-full md:w-auto md:justify-start mb-0 md:mb-0">
+              <Acciones
+                Ver={handleVerClick}
+                ImprimePDF={ImprimePDF}
+                ImprimeExcel={ImprimeExcel}
+                home={home}
+                isLoading={animateLoading}
+                permiso_imprime={permissions.impresion}
+                CerrarView={CerrarView}>              
+              </Acciones>
+            </div>
+            <h1 className="order-1 md:order-2 text-4xl font-xthin text-black dark:text-white mb-5 md:mb-0 grid grid-flow-col gap-1 justify-around mx-5">
+              Reporte de Becas.
+            </h1>
+          </div>
+        </div>
+        <div className="flex flex-col items-center h-full">
+          <div className="w-full max-w-4xl space-y-4">
+            <div className="w-full">
+              <label className="text-sm text-black dark:text-white">Alumno 1:</label>
+              <BuscarCat
+                table="alumnos"
+                token={session.user.token}
+                nameInput={["numero", "nombre"]}
+                fieldsToShow={["numero", "nombre"]}
+                setItem={setHorario1}
+                modalId="modal_horarios"
+              />
+            </div>
+            <div className="w-full">
+              <label className="text-sm text-black dark:text-white">Alumno 2:</label>
+              <BuscarCat
+                table="alumnos"
+                token={session.user.token}
+                nameInput={["numero_2", "nombre_2"]}
+                fieldsToShow={["numero", "nombre"]}
+                setItem={setHorario2}
+                modalId="modal_horarios2"
+              />
+            </div>
+            <div className="flex flex-col space-y-2 md:space-y-0 md:flex-row md:items-center md:space-x-4">
+              <span className="text-black dark:text-white">Ordenar por:</span>
+              <div className="flex items-center space-x-2">
+                <input
+                  type="radio"
+                  name="ordenar"
+                  value="nombre"
+                  onChange={handleCheckChange}
+                  checked={sOrdenar === "nombre"}
+                  className="radio checked:bg-blue-500"
+                />
+                <span className="text-black dark:text-white">Nombre</span>
+              </div>
+              <div className="flex items-center space-x-2">
+                <input
+                  type="radio"
+                  name="ordenar"
+                  value="id"
+                  onChange={handleCheckChange}
+                  checked={sOrdenar === "id"}
+                  className="radio checked:bg-blue-500"
+                />
+                <span className="text-black dark:text-white">Número</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="container w-full max-w-screen-xl bg-base-200 dark:bg-slate-700 shadow-xl rounded-xl px-3">
         <div className="flex justify-start p-3">
             <h1 className="text-4xl font-xthin text-black dark:text-white md:px-12">
                 Reporte de Becas.
@@ -237,9 +320,9 @@ function RepBecas(){
                 </div>
             </div>
         </div>
-    </div>
-</>
-);
+    </div> */}
+    </>
+  );
 }
 
 export default RepBecas;
