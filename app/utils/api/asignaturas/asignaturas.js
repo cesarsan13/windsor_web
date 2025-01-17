6bea63b75025803ab5c2c94b0f5c6fc11c86a93d
AndreaMejia3 <andreamejia3333@outlook.com>
2025-01-17 17:16:46 -0600

    Implementacion de ProcesoExcel en Asignaturas 05:16pm 17/01/2025

--- app/utils/api/asignaturas/asignaturas.js
+++ app/utils/api/asignaturas/asignaturas.js
@@ -159,3 +159,19 @@ export const ImprimirExcel = (configuracion) => {
   const timeStr = formatTime(date).replace(/:/g, "");
   newExcel.guardaReporte(`${nombre}${dateStr}${timeStr}`);
 };
+
+export const storeBatchAsignatura = async (token, data) => {
+  console.log("data", data);
+  let url = `${process.env.DOMAIN_API}api/subject/Batch`;
+  const res = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: new Headers({
+      Authorization: "Bearer " + token,
+      xescuela: localStorage.getItem("xescuela"),
+      "Content-Type": "application/json",
+    }),
+  });
+  const resJson = await res.json();
+  return resJson.data;
+};
