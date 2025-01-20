d96fd386dc0890dba32c6638fe7be26c111f04a1
cesarsan13 <cesaromarsancheztapia@gmail.com>
2025-01-20 14:12:57 -0600

    cambios daniel

--- app/utils/api/productos/productos.js
+++ app/utils/api/productos/productos.js
@@ -159,13 +159,9 @@ export const Imprimir = (configuracion) => {
   const dateStr = formatDate(date);
   const timeStr = formatTime(date);
   // console.log("dateStr:",dateStr," timeStr:",timeStr)
-<<<<<<< HEAD
-  newPDF.guardaReporte(`Productos_${dateStr.replaceAll("/","")}_${timeStr.replaceAll(":","")}`);
-=======
   newPDF.guardaReporte(
     `Productos_${dateStr.replaceAll("/", "")}_${timeStr.replaceAll(":", "")}`
   );
->>>>>>> origin/Fer
 };
 
 export const ImprimirExcel = (configuracion) => {
@@ -176,9 +172,10 @@ export const ImprimirExcel = (configuracion) => {
   newExcel.setColumnas(columns);
   newExcel.setCondition("cam_precio", (value) => value === 1);
   newExcel.addData(body);
-  const date = new Date()
-  const dateStr = formatDate(date)
-  const timeStr = formatTime(date)  
-  newExcel.guardaReporte(`${nombre}_${dateStr.replaceAll("/","")}_${timeStr.replaceAll(":","")}`);
-
+  const date = new Date();
+  const dateStr = formatDate(date);
+  const timeStr = formatTime(date);
+  newExcel.guardaReporte(
+    `${nombre}_${dateStr.replaceAll("/", "")}_${timeStr.replaceAll(":", "")}`
+  );
 };
