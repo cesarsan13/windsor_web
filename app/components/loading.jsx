f8d4c87e2c1a32b3669fb58134bfa09aa2b80ded
AndreaMejia3 <andreamejia3333@outlook.com>
2025-02-24 15:14:09 -0600

    Cambio en el loading 03:13pm 24/02/2025

--- app/components/loading.jsx
+++ app/components/loading.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Loading() {
   return (
     <div className="flex items-center md:-mt-10 mt-10 rounded-3xl">
-      <div className="bg-salte-100 rounded-3xl">
-        <div className=" md:p-52 p-10 flex flex-col rounded-3xl h-5/6">
+      <div className="w-full bg-salte-100 rounded-3xl">
+        <div className="md:p-52 p-10 flex flex-col rounded-3xl h-5/6">
           <div className="flex items-center justify-center">
             <span className="loading loading-ring h-52 w-52 text-black dark:text-white"></span>
           </div>
