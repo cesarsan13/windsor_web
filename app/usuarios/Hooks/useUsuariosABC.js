9a6941f6f164044e04ef183fea26937fefe9ef01
AndreaMejia3 <andreamejia3333@outlook.com>
2025-02-24 13:41:17 -0600

    Cambios al nuevo formato al modulo de Usuarios Ya funciona 12:26pm 24/02/2025

---
+++ app/usuarios/Hooks/useUsuariosABC.js
@@ -0,0 +1,371 @@
+import { useCallback, useRef, useMemo } from "react";
+import { useForm } from "react-hook-form";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { inactiveActiveBaja } from "@/app/utils/GlobalApis";
+import { debounce, permissionsComponents } from "@/app/utils/globalfn";
+import {
+    getUsuarios,
+    guardaUsuario,
+} from "@/app/utils/api/usuarios/usuarios";
+import { showSwal, confirmSwal} from "@/app/utils/alerts";
+
+export const useUsuariosABC = () => {
+    const router = useRouter();
+    const { data: session, status } = useSession();
+    const [usuarios, setUsuarios] = useState([]);
+    const [usuario, setUsuario] = useState({});
+    const [usuariosFiltrados, setUsuariosFiltrados] = useState(null);
+    const [inactiveActive, setInactiveActive] = useState([]);
+    const [bajas, setBajas] = useState(false);
+    const [openModal, setModal] = useState(false);
+    const [accion, setAccion] = useState("");
+    const [isLoading, setisLoading] = useState(false);
+    const [isLoadingButton, setisLoadingButton] = useState(false);
+    const [currentID, setCurrentId] = useState("");
+    const usuariosRef = useRef(usuarios);
+    const [permissions, setPermissions] = useState({});
+    const [titulo, setTitulo] = useState("");
+    const [isDisabled, setIsDisabled] = useState(true);
+    const [reload_page, setReloadPage] = useState(false);
+    const [active, setActive] = useState(false);
+    const [inactive, setInactive] = useState(false);
+    const [busqueda, setBusqueda] = useState({
+        tb_id: "",
+        tb_name: "",
+    });
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+        watch,
+    } = useForm({
+        defaultValues: {
+          id: usuario.id,
+          nombre: usuario.nombre,
+          name: usuario.name,
+          email: usuario.email,
+          password: usuario.password,
+          match_password: usuario.match_password,
+        },
+    });
+
+      useEffect(() => {
+        const fetchData = async () => {
+          setisLoading(true);
+          const { token, permissions } = session.user;
+          const es_admin = session.user.es_admin || false;
+          const menuSeleccionado = Number(localStorage.getItem("puntoMenu"));
+          const busqueda = limpiarBusqueda();
+          const data = await getUsuarios(token, bajas);
+          const res = await inactiveActiveBaja(session?.user.token, "users");
+          setUsuarios(data);
+          setUsuariosFiltrados(data);
+          setInactiveActive(res.data);
+          const permisos = permissionsComponents(
+            es_admin,
+            permissions,
+            session.user.id,
+            menuSeleccionado
+          );
+          await fetchUsuariosStatus(res.data, busqueda);
+          setPermissions(permisos);
+          setisLoading(false);
+        };
+      
+        if (status === "loading" || !session) {
+          return;
+        }
+        fetchData();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [ status, bajas, reload_page]);
+
+    useEffect(() => {
+        usuariosRef.current = usuarios;
+    }, [usuarios]);
+
+    const Buscar = useCallback(async () => {
+        const { tb_id, tb_name } = busqueda;
+    
+        if (tb_id === "" && tb_name === "") {
+          setUsuariosFiltrados(usuariosRef.current);
+          await fetchUsuariosStatus(inactiveActive, busqueda);
+          return;
+        }
+        const infoFiltrada = usuariosRef.current.filter((fusuarios) => {
+          const coincideID = tb_id
+            ? fusuarios["id"].toString().includes(tb_id)
+            : true;
+          const coincideusuario = tb_name
+            ? fusuarios["name"]
+                .toString()
+                .toLowerCase()
+                .includes(tb_name.toLowerCase())
+            : true;
+          return coincideID && coincideusuario;
+        });
+        setUsuariosFiltrados(infoFiltrada);
+        await fetchUsuariosStatus(inactiveActive, busqueda);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [busqueda]);
+
+    const debouncedBuscar = useMemo(() => debounce(Buscar, 500), [Buscar]);
+
+    const fetchUsuariosStatus = async (
+        inactiveActive,
+        busqueda
+    ) => {
+        const { tb_id, tb_name } = busqueda;
+
+        let infoFiltrada = [];
+        let active = 0;
+        let inactive = 0;
+
+        if (tb_id || tb_name) {
+            infoFiltrada = inactiveActive.filter((fusuarios) => {
+                const coincideID = tb_id
+                  ? fusuarios["id"].toString().includes(tb_id)
+                  : true;
+                const coincideusuario = tb_name
+                  ? fusuarios["name"]
+                      .toString()
+                      .toLowerCase()
+                      .includes(tb_name.toLowerCase())
+                  : true;
+                return coincideID && coincideusuario;
+            });
+            active = infoFiltrada.filter((c) => c.baja !== "*").length;
+            inactive = infoFiltrada.filter((c) => c.baja === "*").length;
+        } else {
+            active = inactiveActive.filter((c) => c.baja !== "*").length;
+            inactive = inactiveActive.filter((c) => c.baja === "*").length;
+        }
+        setActive(active);
+        setInactive(inactive);
+    };
+
+    useEffect(() => {
+        debouncedBuscar();
+        return () => {
+          clearTimeout(debouncedBuscar);
+        };
+    }, [busqueda, debouncedBuscar]);
+
+    useEffect(() => {
+        const debouncedBuscar = debounce(Buscar, 500);
+        debouncedBuscar();
+        return () => {
+          clearTimeout(debouncedBuscar);
+        };
+    }, [busqueda, Buscar]);
+
+    useEffect(() => {
+        if (accion === "Eliminar" || accion === "Ver") {
+          setIsDisabled(true);
+        }
+        if (accion === "Alta" || accion === "Editar") {
+          setIsDisabled(false);
+        }
+        setTitulo(
+          accion === "Alta"
+            ? `Nuevo Usuario`
+            : accion === "Editar"
+            ? `Editar Usuario: ${currentID}`
+            : accion === "Eliminar"
+            ? `Eliminar Usuario: ${currentID}`
+            : `Ver Usuario: ${currentID}`
+        );
+    }, [accion, currentID]);
+
+    useEffect(() => {
+        reset({
+          id: usuario.id,
+          nombre: usuario.nombre,
+          name: usuario.name,
+          email: usuario.email,
+          password: usuario.password,
+          match_password: usuario.match_password,
+        });
+    }, [usuario, reset]);
+
+    const limpiarBusqueda = () => {
+        const search = {tb_id: "", tb_name: ""};
+        setBusqueda({ tb_id: "", tb_name: "" });
+        return search;
+    };
+
+    const Alta = async () => {
+        setCurrentId("");
+        reset({
+          id: "",
+          name: "",
+          nombre: "",
+          email: "",
+          password: "",
+          match_password: "",
+        });
+        setUsuario({ id: "" });
+        setModal(!openModal);
+        setAccion("Alta");
+        showModal(true);
+        document.getElementById("name").focus();
+    };
+
+    const onSubmitModal = handleSubmit(async (data) => {
+        event.preventDefault();
+        setisLoadingButton(true);
+        accion === "Alta" ? (data.id = "") : (data.id = currentID);
+        const password1 = watch("password", "");
+        const password2 = watch("match_password", "");
+    
+        if (password1 !== password2) {
+            showSwal(
+                "Error de Validación",
+                "Las contraseñas no coinciden",
+                "error",
+                "my_modal_3"
+            );
+            setisLoadingButton(false);
+            return;
+        } else {
+            if (
+                (accion === "Alta" || accion === "Editar") &&
+                (password1 === "" || password2 === "")
+            ) {
+                showSwal(
+                "Error de Validación",
+                "Por favor capture las contraseñas",
+                "error",
+                "my_modal_3"
+                );
+                setisLoadingButton(false);
+                return;
+            }
+    
+            let res = null;
+    
+            if (accion === "Eliminar") {
+                showModal(false);
+                const confirmed = await confirmSwal(
+                "¿Desea Continuar?",
+                "Se eliminara al usuario seleccionado",
+                "warning",
+                "Aceptar",
+                "Cancelar",
+                );
+                if (!confirmed) {
+                    showModal(true);
+                    setisLoadingButton(false);
+                    return;
+                }
+            }
+    
+            data.numero_prop = 1;
+    
+            res = await guardaUsuario(session.user.token, data, accion);
+            if (res.status) {
+                if (accion === "Alta") {
+                    data.id = res.data;
+                    setCurrentId(data.id);
+                    const nuevoUsuarios = { currentID, ...data };
+                    setUsuarios([...usuarios, nuevoUsuarios]);
+                    if (!bajas) {
+                        setUsuariosFiltrados([...usuariosFiltrados, nuevoUsuarios]);
+                    }
+                }
+                if (accion === "Eliminar" || accion === "Editar") {
+                    const index = usuarios.findIndex((fp) => fp.id === data.id);
+                    if (index !== -1) {
+                        if (accion === "Eliminar") {
+                            const fpFiltrados = usuarios.filter((fp) => fp.id !== data.id);
+                            setUsuarios(fpFiltrados);
+                            setUsuariosFiltrados(fpFiltrados);
+                        } else {
+                            if (bajas) {
+                                const fpFiltrados = usuarios.filter((fp) => fp.id !== data.id);
+                                setUsuarios(fpFiltrados);
+                                setUsuariosFiltrados(fpFiltrados);
+                            } else {
+                                const fpActualizadas = usuarios.map((fp) =>
+                                    fp.id === currentID ? { ...fp, ...data } : fp
+                                );
+                                setUsuarios(fpActualizadas);
+                                setUsuariosFiltrados(fpActualizadas);
+                            }
+                        }
+                    }
+                }
+                showSwal(res.alert_title, res.alert_text, res.alert_icon);
+                showModal(false);
+            } else {
+                showSwal(res.alert_title, res.alert_text, res.alert_icon, "my_modal_3");
+            }
+            if (accion === "Alta" || accion === "Eliminar") {
+                setReloadPage(!reload_page);
+                await fetchUsuariosStatus(inactiveActive, busqueda);
+            }
+            setisLoadingButton(false);
+        }
+    });
+
+    const showModal = (show) => {
+        show
+          ? document.getElementById("my_modal_3").showModal()
+          : document.getElementById("my_modal_3").close();
+    };
+    
+    const home = () => {
+        router.push("/");
+    };
+      
+    const handleBusquedaChange = (event) => {
+        event.preventDefault();
+        setBusqueda((estadoPrevio) => ({
+          ...estadoPrevio,
+          [event.target.id]: event.target.value,
+        }));
+    };
+
+    const tableAction = (evt, usuario, accion) => {
+        evt.preventDefault();
+        setUsuario(usuario);
+        setAccion(accion);
+        setCurrentId(usuario.id);
+        showModal(true);
+    };
+
+
+    return{
+        onSubmitModal,
+        Buscar,
+        Alta,
+        home,
+        setBajas,
+        limpiarBusqueda,
+        handleBusquedaChange,
+        fetchUsuariosStatus,
+        setReloadPage,
+        setisLoadingButton,
+        tableAction,
+        register,
+        status,
+        session,
+        isLoadingButton,
+        isLoading,
+        accion,
+        permissions,
+        active,
+        inactive,
+        busqueda,
+        usuariosFiltrados,
+        titulo,
+        reload_page,
+        isDisabled,
+        errors,
+        inactiveActive,
+    };
+
+};
--- app/usuarios/page.jsx
+++ app/usuarios/page.jsx
@@ -1,383 +1,82 @@
 "use client";
-import React, { useCallback, useRef, useMemo } from "react";
-import { useRouter } from "next/navigation";
-import { showSwal, confirmSwal, showSwalTarget } from "@/app/utils/alerts";
-import ModalUsuarios from "./components/ModalUsuario";
-import Busqueda from "./components/Busqueda";
-import Acciones from "./components/Acciones";
-import { useForm } from "react-hook-form";
-import {
-  getUsuarios,
-  ImprimirPDF,
-  ImprimirExcel,
-  guardaUsuario,
-} from "../utils/api/usuarios/usuarios";
-import { useState, useEffect } from "react";
-import { useSession } from "next-auth/react";
-import { siguiente } from "@/app/utils/api/comentarios/comentarios";
+import React from "react";
+import ModalComponent from "@/app/components/Catalogs_Components/modalComponent";
+import TableComponent from "@/app/components/Catalogs_Components/tableComponent";
+import Busqueda from "@/app/usuarios/components/Busqueda";
+import Acciones from "@/app/usuarios/components/Acciones";
+import VistaPrevia from "@/app/components/VistaPrevia";
+import { useUsuariosABC } from "@/app/usuarios/Hooks/useUsuariosABC";
+import { useUsuariosPdfExcel } from "/app/usuarios/Hooks/useUsuariosPdfExcel";
+import { useUsuariosUI } from "/app/usuarios/Hooks/useUsuariosUI";
 import "jspdf-autotable";
-import { ReportePDF } from "@/app/utils/ReportesPDF";
 import "@react-pdf-viewer/core/lib/styles/index.css";
-import TablaUsuarios from "./components/TablaUsuarios";
-import { debounce, permissionsComponents } from "../utils/globalfn";
-import VistaPrevia from "../components/VistaPrevia";
+
 function Usuarios() {
-  const router = useRouter();
-  const { data: session, status } = useSession();
-  const [usuarios, setUsuarios] = useState([]);
-  const [usuario, setUsuario] = useState({});
-  const [usuariosFiltrados, setUsuariosFiltrados] = useState(null);
-  const [animateLoading, setAnimateLoading] = useState(false);
-
-  const [bajas, setBajas] = useState(false);
-  const [openModal, setModal] = useState(false);
-  const [accion, setAccion] = useState("");
-  const [isLoading, setisLoading] = useState(false);
-  const [currentID, setCurrentId] = useState("");
-  const [pdfPreview, setPdfPreview] = useState(false);
-  const [pdfData, setPdfData] = useState("");
-  const usuariosRef = useRef(usuarios);
-  const [busqueda, setBusqueda] = useState({
-    tb_id: "",
-    tb_name: "",
-  });
-  const [permissions, setPermissions] = useState({});
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setisLoading(true);
-      const { token, permissions } = session.user;
-      const es_admin = session.user.es_admin;
-      const menuSeleccionado = Number(localStorage.getItem("puntoMenu"));
-  
-      let data = await getUsuarios(token, bajas);
-      
-      if (!es_admin) {
-        data = data.filter((user) => user.id === session.user.id);
-      }
-  
-      setUsuarios(data);
-      setUsuariosFiltrados(data);
-      setisLoading(false);
-  
-      const permisos = permissionsComponents(
-        es_admin,
-        permissions,
-        session.user.id,
-        menuSeleccionado
-      );
-      setPermissions(permisos);
-    };
-  
-    if (status === "loading" || !session) {
-      return;
-    }
-    fetchData();
-  }, [session, status, bajas]);
-  
-  useEffect(() => {
-    usuariosRef.current = usuarios;
-  }, [usuarios]);
-  const Buscar = useCallback(() => {
-    const { tb_id, tb_name } = busqueda;
-
-    if (tb_id === "" && tb_name === "") {
-      setUsuariosFiltrados(usuariosRef.current);
-      return;
-    }
-    const infoFiltrada = usuariosRef.current.filter((fusuarios) => {
-      const coincideID = tb_id
-        ? fusuarios["id"].toString().includes(tb_id)
-        : true;
-      const coincideusuario = tb_name
-        ? fusuarios["name"]
-            .toString()
-            .toLowerCase()
-            .includes(tb_name.toLowerCase())
-        : true;
-      return coincideID && coincideusuario;
-    });
-    setUsuariosFiltrados(infoFiltrada);
-  }, [busqueda]);
-
-  const debouncedBuscar = useMemo(() => debounce(Buscar, 500), [Buscar]);
-  useEffect(() => {
-    debouncedBuscar();
-    return () => {
-      clearTimeout(debouncedBuscar);
-    };
-  }, [busqueda, debouncedBuscar]);
-
   const {
+    onSubmitModal,
+    Buscar,
+    Alta,
+    home,
+    setBajas,
+    limpiarBusqueda,
+    handleBusquedaChange,
+    tableAction,
     register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-    watch,
-  } = useForm({
-    defaultValues: {
-      id: usuario.id,
-      nombre: usuario.nombre,
-      name: usuario.name,
-      email: usuario.email,
-      password: usuario.password,
-      match_password: usuario.match_password,
-    },
-  });
-  useEffect(() => {
-    reset({
-      id: usuario.id,
-      nombre: usuario.nombre,
-      name: usuario.name,
-      email: usuario.email,
-      password: usuario.password,
-      match_password: usuario.match_password,
-    });
-  }, [usuario, reset]);
+    status,
+    session,
+    isLoadingButton,
+    isLoading,
+    accion,
+    permissions,
+    active,
+    inactive,
+    busqueda,
+    usuariosFiltrados,
+    titulo,
+    isDisabled,
+    errors,
+  } = useUsuariosABC();
 
-  const limpiarBusqueda = (evt) => {
-    evt.preventDefault();
-    setBusqueda({ tb_id: "", tb_name: "" });
-  };
+  const{
+    handleVerClick,
+    CerrarView,
+    ImprimePDF,
+    ImprimeExcel,
+    pdfPreview,
+    pdfData,
+    animateLoading,
+  } = useUsuariosPdfExcel(
+    usuariosFiltrados,
+    session,
+  );
 
-  const handleVerClick = () => {
-    setAnimateLoading(true);
-    const configuracion = {
-      Encabezado: {
-        Nombre_Aplicacion: "Sistema de Control Escolar",
-        Nombre_Reporte: "Reporte de Usuarios",
-        Nombre_Usuario: `Usuario: ${session.user.name}`,
-      },
-      body: usuariosFiltrados,
-    };
+  const{
+    tableColumns,
+    tableBody,
+    modalBody
+  } = useUsuariosUI(
+    tableAction,
+    register,
+    permissions,
+    isDisabled,
+    errors
+  );
 
-    const orientacion = "Portrait";
-    const reporte = new ReportePDF(configuracion, orientacion);
-    const { body } = configuracion;
-    const Enca1 = (doc) => {
-      if (!doc.tiene_encabezado) {
-        doc.imprimeEncabezadoPrincipalV();
-        doc.nextRow(12);
-        doc.ImpPosX("Id", 15, doc.tw_ren);
-        doc.ImpPosX("Usuario", 30, doc.tw_ren);
-        doc.ImpPosX("Nombre", 70, doc.tw_ren);
-        doc.ImpPosX("Email", 150, doc.tw_ren);
-
-        doc.nextRow(4);
-        doc.printLineV();
-        doc.nextRow(4);
-        doc.tiene_encabezado = true;
-      } else {
-        doc.nextRow(6);
-        doc.tiene_encabezado = true;
-      }
-    };
-
-    Enca1(reporte);
-    body.forEach((usuarios) => {
-      reporte.ImpPosX(usuarios.id.toString(), 20, reporte.tw_ren, 0, "R");
-      reporte.ImpPosX(usuarios.name.toString(), 30, reporte.tw_ren, 35, "L");
-      reporte.ImpPosX(usuarios.nombre.toString(), 70, reporte.tw_ren, 35, "L");
-      reporte.ImpPosX(usuarios.email.toString(), 150, reporte.tw_ren, 35, "L");
-      Enca1(reporte);
-      if (reporte.tw_ren >= reporte.tw_endRen) {
-        reporte.pageBreak();
-        Enca1(reporte);
-      }
-    });
-
-    setTimeout(() => {
-      const pdfData = reporte.doc.output("datauristring");
-      setPdfData(pdfData);
-      setPdfPreview(true);
-      showModalVista(true);
-      setAnimateLoading(false);
-    }, 500);
-  };
-
-  const showModalVista = (show) => {
-    show
-      ? document.getElementById("modalVPUsuario").showModal()
-      : document.getElementById("modalVPUsuario").close();
-  };
-
-  const CerrarView = () => {
-    setPdfPreview(false);
-    setPdfData("");
-    document.getElementById("modalVPUsuario").close();
-  };
-
-  const Alta = async (event) => {
-    setCurrentId("");
-    const { token } = session.user;
-    reset({
-      id: "",
-      name: "",
-      nombre: "",
-      email: "",
-      password: "",
-      match_password: "",
-    });
-
-    setUsuario({ id: "" });
-    setModal(!openModal);
-    setAccion("Alta");
-    showModal(true);
-
-    document.getElementById("name").focus();
-  };
-
-  const ImprimePDF = () => {
-    const configuracion = {
-      Encabezado: {
-        Nombre_Aplicacion: "Sistema de Control Escolar",
-        Nombre_Reporte: "Reporte de Usuarios",
-        Nombre_Usuario: `Usuario: ${session.user.name}`,
-      },
-      body: usuariosFiltrados,
-    };
-    ImprimirPDF(configuracion);
-  };
-
-  const ImprimeExcel = () => {
-    const configuracion = {
-      Encabezado: {
-        Nombre_Aplicacion: "Sistema de Control Escolar",
-        Nombre_Reporte: "Reporte de Usuarios",
-        Nombre_Usuario: `Usuario: ${session.user.name}`,
-      },
-
-      body: usuariosFiltrados,
-      columns: [
-        { header: "Id", dataKey: "id" },
-        { header: "Usuario", dataKey: "name" },
-        { header: "Nombre", dataKey: "nombre" },
-        { header: "Email", dataKey: "email" },
-      ],
-      nombre: "Usuarios",
-    };
-    ImprimirExcel(configuracion);
-  };
-
-  const onSubmitModal = handleSubmit(async (data) => {
-    event.preventDefault;
-
-    const password1 = watch("password", "");
-    const password2 = watch("match_password", "");
-
-    if (password1 !== password2) {
-      showSwal(
-        "Error de Validación",
-        "Las contraseñas no coinciden",
-        "error",
-        "modal_usuarios"
-      );
-      return;
-    } else {
-      if (
-        (accion === "Alta" || accion === "Editar") &&
-        (password1 === "" || password2 === "")
-      ) {
-        showSwal(
-          "Error de Validación",
-          "Por favor capture las contraseñas",
-          "error",
-          "modal_usuarios"
-        );
-        return;
-      }
-
-      let res = null;
-
-      if (accion === "Eliminar") {
-        const confirmed = await confirmSwal(
-          "¿Desea Continuar?",
-          "Se eliminara al usuario seleccionado",
-          "warning",
-          "Aceptar",
-          "Cancelar",
-          "modal_usuarios"
-        );
-        if (!confirmed) {
-          return;
-        }
-      }
-
-      data.numero_prop = 1;
-
-      res = await guardaUsuario(session.user.token, data, accion);
-      if (res.status) {
-        if (accion === "Alta") {
-          data.id = res.data;
-          setCurrentId(data.id);
-          const nuevoUsuarios = { currentID, ...data };
-          setUsuarios([...usuarios, nuevoUsuarios]);
-          if (!bajas) {
-            setUsuariosFiltrados([...usuariosFiltrados, nuevoUsuarios]);
-          }
-        }
-        if (accion === "Eliminar" || accion === "Editar") {
-          const index = usuarios.findIndex((fp) => fp.id === data.id);
-          if (index !== -1) {
-            if (accion === "Eliminar") {
-              const fpFiltrados = usuarios.filter((fp) => fp.id !== data.id);
-              setUsuarios(fpFiltrados);
-              setUsuariosFiltrados(fpFiltrados);
-            } else {
-              if (bajas) {
-                const fpFiltrados = usuarios.filter((fp) => fp.id !== data.id);
-                setUsuarios(fpFiltrados);
-                setUsuariosFiltrados(fpFiltrados);
-              } else {
-                const fpActualizadas = usuarios.map((fp) =>
-                  fp.id === currentID ? { ...fp, ...data } : fp
-                );
-                setUsuarios(fpActualizadas);
-                setUsuariosFiltrados(fpActualizadas);
-              }
-            }
-          }
-        }
-        showSwal(res.alert_title, res.alert_text, res.alert_icon);
-        showModal(false);
-      }
-    }
-  });
-
-  const showModal = (show) => {
-    show
-      ? document.getElementById("modal_usuarios").showModal()
-      : document.getElementById("modal_usuarios").close();
-  };
-
-  const home = () => {
-    router.push("/");
-  };
-  const handleBusquedaChange = (event) => {
-    event.preventDefault;
-    setBusqueda((estadoPrevio) => ({
-      ...estadoPrevio,
-      [event.target.id]: event.target.value,
-    }));
-  };
-
-  if (status === "loading") {
+  if (status === "loading" && !session) {
     return (
       <div className="container skeleton    w-full  max-w-screen-xl  shadow-xl rounded-xl "></div>
     );
   }
   return (
     <>
-      <ModalUsuarios
-        accion={accion}
-        onSubmit={onSubmitModal}
-        currentID={currentID}
-        errors={errors}
-        register={register}
-        setUsuario={setUsuario}
-        Usuario={usuario}
-        watch={watch}
+      <ModalComponent
+          accion={accion}
+          onSubmit={onSubmitModal}
+          isLoadingButton={isLoadingButton}
+          titulo={titulo}
+          modalBody={modalBody}
       />
+
       <VistaPrevia
         id="modalVPUsuario"
         titulo={"Vista Previa de Usuarios"}
@@ -400,12 +99,16 @@ function Usuarios() {
                 animateLoading={animateLoading}
                 permiso_alta={permissions.altas}
                 permiso_imprime={permissions.impresion}
-              ></Acciones>
+              />
             </div>
 
             <h1 className="order-1 md:order-2 text-4xl font-xthin text-black dark:text-white mb-5 md:mb-0 grid grid-flow-col gap-1 justify-around mx-5">
               Usuarios
             </h1>
+            <h3 className="ml-auto order-3">{`Usuarios activos: ${
+              active || 0
+              }\nUsuarios inactivos: ${inactive || 0}`}
+            </h3>
           </div>
         </div>
 
@@ -418,16 +121,12 @@ function Usuarios() {
               handleBusquedaChange={handleBusquedaChange}
               busqueda={busqueda}
             />
-            <TablaUsuarios
+            <TableComponent
+              data={usuariosFiltrados}
               session={session}
               isLoading={isLoading}
-              usuariosFiltrados={usuariosFiltrados}
-              showModal={showModal}
-              setUsuario={setUsuario}
-              setAccion={setAccion}
-              setCurrentId={setCurrentId}
-              permiso_cambio={permissions.cambios}
-              permiso_baja={permissions.bajas}
+              tableColumns={tableColumns}
+              tableBody={tableBody}
             />
           </div>
         </div>
