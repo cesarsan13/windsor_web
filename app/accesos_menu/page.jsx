3bb6e021a24b8b191e3f88cfac2a174669157652
AndreaMejia3 <andreamejia3333@outlook.com>
2025-02-25 13:41:04 -0600

    Cambio en la vista de accesos_menu 01:40pm 25/02/2025

--- app/accesos_menu/page.jsx
+++ app/accesos_menu/page.jsx
@@ -55,7 +55,7 @@ function Accesos_Menu() {
         titulo={titulo}
         modalBody={modalBody}
       />
-      <div className="container h-[80vh] w-full max-w-screen-xl bg-base-200 dark:bg-slate-700 shadow-xl rounded-xl px-3 md:overflow-y-auto lg:overflow-y-hidden">
+      <div className="container h-[85vh] w-full max-w-[1800px] bg-base-200 dark:bg-slate-700 shadow-xl rounded-xl px-3 md:overflow-y-auto lg:overflow-y-hidden">
         <div className="flex flex-col justify-start p-3">
           <div className="flex flex-wrap md:flex-nowrap items-start md:items-center">
             <div className="order-2 md:order-1 flex justify-around w-full md:w-auto md:justify-start mb-0 md:mb-0">
@@ -74,7 +74,7 @@ function Accesos_Menu() {
           </div>
         </div>
         <div className="flex flex-col items-center h-full">
-          <div className="w-full max-w-4xl">
+          <div className="w-full max-w-full">
             <Busqueda
               setBajas={setBajas}
               limpiarBusqueda={limpiarBusqueda}
